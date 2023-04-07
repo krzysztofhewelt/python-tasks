05f83e8b49381048009b94c748aa4d397b3a1864
Krzysztof Hewelt <krzysiu-nv@wp.pl>
2023-04-07 15:17:52 +0200

    Refactor code (not tested yet)

--- tkinter-gui/main.py
+++ tkinter-gui/main.py
@@ -10,16 +10,135 @@ import pandas as pd
 from conn import Database
 
 
+class Validator:
+
+    def validate_types_of_measurements(self, temperature, humidity, voltage):
+        if not temperature and not humidity and not voltage:
+            return False
+            self.errors += "- You must choose at least one type of measurement\n"
+
+        return True
+
+    def check_device_id(self, device_name):
+        if Devices.get_device_id(device_name) is None:
+            return False
+            self.errors += "- Device not found\n"
+
+        return True
+
+    def check_start_date(self, start):
+        if start:
+            try:
+                datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
+                return True
+            except ValueError:
+                return False
+                self.errors += "- Start time is not valid date!\n"
+
+        return None
+
+    def check_stop_date(self, stop):
+        if stop:
+            try:
+                datetime.strptime(stop, "%Y-%m-%d %H:%M:%S")
+                return True
+            except ValueError:
+                return False
+                self.errors += "- Stop time is not valid date!\n"
+
+        return None
+
+    def validate_dates(self, start, stop):
+        validated_start = self.check_start_date(start)
+        validated_stop = self.check_stop_date(stop)
+
+        if validated_start is False or validated_stop is False:
+            return False
+
+        if self.check_start_date(start) and self.check_stop_date(stop):
+            start_date = datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
+            stop_date = datetime.strptime(stop, "%Y-%m-%d %H:%M:%S")
+
+            if start_date and stop_date and start_date > stop_date:
+                return False
+                self.errors += "- Start time must be less or equal to stop time\n"
+
+        return True
+
+    def validate_all(self, device_name, temperature, humidity, voltage, start, stop):
+        return self.validate_types_of_measurements(temperature, humidity, voltage) and self.check_device_id(
+            device_name) and self.validate_dates(start, stop)
+
+
+class Devices:
+    devices = dict()
+
+    def __init__(self):
+        self.conn = Database()
+        self.get_devices_from_database()
+
+    def get_devices_from_database(self):
+        devices = self.conn.fetch("SELECT id, name FROM devices")
+        for device in devices:
+            self.devices[device[1]] = device[0]
+
+    def get_device_names_list(self):
+        return list(self.devices.keys())
+
+    @staticmethod
+    def get_device_id(name):
+        try:
+            return Devices.devices[name]
+        except KeyError:
+            return None
+
+    def make_data_columns_list(self, temperature, humidity, voltage):
+        columns = ['time']
+
+        if temperature:
+            columns.append('temperature')
+
+        if voltage:
+            columns.append('voltage')
+
+        if humidity:
+            columns.append('humidity')
+
+        return columns
+
+    def device_probes_from_database(self, device_name, temperature, humidity, voltage, start, stop):
+        print(device_name)
+        dev_id = self.get_device_id(str(device_name))
+        start_time = start
+        stop_time = stop
+        columns = self.make_data_columns_list(temperature, humidity, voltage)
+        columns_joined = ','.join(columns)
+
+        query = f"SELECT {columns_joined} FROM data WHERE device_id = {dev_id} "
+
+        if start_time:
+            query += f"AND time >= '{start_time}'"
+
+        if stop_time:
+            query += f"AND time <= '{stop_time}'"
+
+        data = self.conn.fetch(query)
+        df = pd.DataFrame(data, columns=columns)
+        if df.empty:
+            messagebox.showinfo("No data", "There is nothing to show")
+            return
+
+        return df
+
+
 class MainApplication:
     def __init__(self, master):
         self.master = master
         self.master.geometry("500x250")
         self.master.title("IoT Visualizer")
 
-        self.conn = Database()
-        self.devices = dict()
+        self.devices = Devices()
 
-        self.get_devices_from_database()
         self.make_controls()
 
     def make_controls(self):
@@ -47,57 +166,31 @@ class MainApplication:
 
         label_devices = Label(self.master, text="available devices:")
         label_devices.pack()
-        devices_list = Combobox(self.master, textvariable=device, state="readonly", values=self.get_device_names_list())
+        devices_list = Combobox(self.master, textvariable=device, state="readonly",
+                                values=self.devices.get_device_names_list())
         devices_list.pack()
 
         submit_button = Button(self.master, text='Display chart',
-                               command=partial(self.get_probes_from_database, device, temperature, humidity, voltage,
+                               command=partial(self.test, device, temperature, humidity, voltage,
                                                entry_start, entry_stop),
                                width=20, height=5)
         submit_button.pack()
 
-    def get_devices_from_database(self):
-        devices = self.conn.fetch("SELECT id, name FROM devices")
-        for device in devices:
-            self.devices[device[1]] = device[0]
-
-    def get_device_names_list(self):
-        return list(self.devices.keys())
-
-    def get_device_id(self, name):
-        try:
-            return self.devices[name]
-        except KeyError:
-            return None
-
-    def get_probes_from_database(self, device_name, temperature, humidity, voltage, start, stop):
-        if not self.validate(device_name.get(), temperature.get(), humidity.get(), voltage.get(), start.get(),
-                             stop.get()):
-            return False
-
-        dev_id = self.get_device_id(str(device_name.get()))
-        start_time = start.get()
-        stop_time = stop.get()
-        columns = self.make_data_columns_list(temperature.get(), humidity.get(), voltage.get())
-        columns_joined = ','.join(columns)
-
-        query = f"SELECT {columns_joined} FROM data WHERE device_id = {dev_id} "
-
-        if start_time:
-            query += f"AND time >= '{start_time}'"
-
-        if stop_time:
-            query += f"AND time <= '{stop_time}'"
-
-        data = self.conn.fetch(query)
-        df = pd.DataFrame(data, columns=columns)
-        if df.empty:
-            messagebox.showinfo("No data", "There is nothing to show")
+    def test(self, device, temperature, humidity, voltage, entry_start, entry_stop):
+        validate = Validator()
+        if not validate.validate_all(device.get(), temperature.get(), humidity.get(), voltage.get(), entry_start.get(),
+                                     entry_stop.get()):
+            messagebox.showerror('Error', 'some errors')
             return
 
+        df = self.devices.device_probes_from_database(device.get(), temperature.get(), humidity.get(), voltage.get(),
+                                                      entry_start.get(), entry_stop.get())
         self.draw_chart(df)
 
     def draw_chart(self, df):
+        if not df:
+            return
+
         plt.rcParams["figure.figsize"] = (16, 9)
 
         fig, axs = plt.subplots(nrows=len(df.columns) - 1, squeeze=False, sharex=True)
@@ -130,51 +223,6 @@ class MainApplication:
         axs[0, 0].set_title('Data visualization')
         plt.subplots_adjust(hspace=.0)  # add some space between plots
         plt.show()
-
-    def make_data_columns_list(self, temperature, humidity, voltage):
-        columns = ['time']
-
-        if temperature:
-            columns.append('temperature')
-
-        if voltage:
-            columns.append('voltage')
-
-        if humidity:
-            columns.append('humidity')
-
-        return columns
-
-    def validate(self, device_name, temperature, humidity, voltage, start_time, stop_time):
-        errors = ""
-
-        if not temperature and not humidity and not voltage:
-            errors += "- You must choose at least one type of measurement\n"
-
-        if self.get_device_id(device_name) is None:
-            errors += "- Device not found\n"
-
-        start_date, stop_date = None, None
-        if start_time:
-            try:
-                start_date = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
-            except ValueError:
-                errors += "- Start time is not valid date!\n"
-
-        if stop_time:
-            try:
-                stop_date = datetime.strptime(stop_time, "%Y-%m-%d %H:%M:%S")
-            except ValueError:
-                errors += "- Stop time is not valid date!\n"
-
-        if start_date and stop_date and start_date > stop_date:
-            errors += "- Start time must be less or equal to stop time\n"
-
-        if errors:
-            messagebox.showerror('Error', errors)
-            return False
-
-        return True
 
 
 root = Tk()
